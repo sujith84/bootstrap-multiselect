8718c20adca419e5f6a559e4ab1be9dda4678c7f
Sujith Sreedhar <sujith.sreedhar@bs-shipmanagement.com>
2017-01-03 17:27:51 +0530

    Various Fixes

--- dist/js/bootstrap-multiselect.js
+++ dist/js/bootstrap-multiselect.js
@@ -425,8 +425,9 @@
             templates: {
                 button: '<button type="button" class="multiselect dropdown-toggle" data-toggle="dropdown"><span class="multiselect-selected-text"></span> <b class="caret"></b></button>',
                 ul: '<ul class="multiselect-container dropdown-menu"></ul>',
-                filter: '<li class="multiselect-item multiselect-filter"><div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-search"></i></span><input class="form-control multiselect-search" type="text"></div></li>',
-                filterClearBtn: '<span class="input-group-btn"><button class="btn btn-default multiselect-clear-filter" type="button"><i class="glyphicon glyphicon-remove-circle"></i></button></span>',
+                filter: '<li class="multiselect-item filter"><input class="form-control multiselect-search" type="text"></li>',
+                filterClearBtn: '',
+                // filterClearBtn: '<span class="input-group-btn"><button class="btn btn-default multiselect-clear-filter" type="button"><i class="glyphicon glyphicon-remove-circle"></i></button></span>',
                 li: '<li><a tabindex="0"><label></label></a></li>',
                 divider: '<li class="multiselect-item divider"></li>',
                 liGroup: '<li class="multiselect-item multiselect-group"><label></label></li>'
@@ -501,7 +502,9 @@
                 this.$ul.css({
                     'max-height': this.options.maxHeight + 'px',
                     'overflow-y': 'auto',
-                    'overflow-x': 'hidden'
+                    'overflow-x': 'hidden',
+                    'width': '100%',
+                    'padding': '5px'
                 });
             }
 
@@ -923,6 +926,7 @@
 
             var classes = this.options.optionClass(group);
             $li.addClass(classes);
+            $li.attr("title", groupName);
 
             if (this.options.enableHTML) {
                 $('label b', $li).html(" " + label);
@@ -1408,7 +1412,8 @@
          * Rebuilds the dropdown, the filter and the select all option.
          */
         rebuild: function() {
-            this.$ul.html('');
+            if (this.$select.children().length > 0)
+                this.$ul.html('');  
 
             // Important to distinguish between radios and checkboxes.
             this.options.multiple = this.$select.attr('multiple') === "multiple";
@@ -1708,6 +1713,116 @@
     };
 
     $.fn.multiselect.Constructor = Multiselect;
+
+    $.fn.multiselect.Constructor.prototype.selectAll = (function() {
+        var cached_function = jQuery.fn.multiselect.Constructor.prototype.selectAll;
+
+        return function() {
+            var args = Array.prototype.slice.call(arguments);
+            args[0] = (this.options.enableCollapsibleOptGroups && this.options.multiple) ? false : args[0];
+            cached_function.apply(this, args);
+        };
+    }());
+
+    $.fn.multiselect.Constructor.prototype.deselectAll = (function() {
+        var cached_function = jQuery.fn.multiselect.Constructor.prototype.deselectAll;
+
+        return function() {
+            var args = Array.prototype.slice.call(arguments);
+            args[0] = (this.options.enableCollapsibleOptGroups && this.options.multiple) ? false : args[0];
+            cached_function.apply(this, args);
+        };
+    }());
+
+    $.fn.multiselect.Constructor.prototype.createOptgroup = (function() {
+        var cached_function = jQuery.fn.multiselect.Constructor.prototype.createOptgroup;
+
+        return function() {
+            var args = Array.prototype.slice.call(arguments);
+            var t = args[0];
+            if (this.options.enableCollapsibleOptGroups && this.options.multiple) {
+                var n = e(t).attr("label");
+                var v = e(t).attr("value");
+                var r =
+                    e('<li class="multiselect-item multiselect-group"><a href="javascript:void(0);"><input type="checkbox" value="' + v + '"/><b> ' + n + '<b class="caret"></b></b></a></li>');
+
+                if (this.options.enableClickableOptGroups) {
+                    r.addClass("multiselect-group-clickable")
+                }
+                this.$ul.append(r);
+                if (e(t).is(":disabled")) {
+                    r.addClass("disabled")
+                }
+                e("option", t)
+                    .each(e.proxy(function(e, t) {
+                            this.createOptionValue(t)
+                        },
+                        this))
+            } else {
+                cached_function.apply(this, arguments);
+            }
+        };
+    }());
+
+    $.fn.multiselect.Constructor.prototype.buildDropdownOptions = (function() {
+        var cached_function = jQuery.fn.multiselect.Constructor.prototype.buildDropdownOptions;
+
+        return function() {
+            cached_function.apply(this, arguments);
+
+            if (this.options.enableCollapsibleOptGroups && this.options.multiple) {
+
+                e("li.multiselect-group input", this.$ul).off();
+                e("li.multiselect-group", this.$ul)
+                    .siblings()
+                    .not("li.multiselect-group, li.multiselect-all", this.$ul)
+                    .each(function() {
+                        $(this).toggleClass('hidden', true);
+                    });
+                e("li.multiselect-group", this.$ul)
+                    .on("click",
+                        e.proxy(function(t) {
+                                t.stopPropagation();
+                            },
+                            this));
+                e("li.multiselect-group > a > b", this.$ul)
+                    .on("click",
+                        e.proxy(function(t) {
+                                t.stopPropagation();
+                                var n = e(t.target).closest('li');
+                                var r = n.nextUntil("li.multiselect-group");
+                                var i = true;
+                                r.each(function() {
+                                    i = i && e(this).hasClass('hidden');
+                                });
+                                r.toggleClass('hidden', !i);
+                            },
+                            this));
+                e("li.multiselect-group > a > input", this.$ul)
+                    .on("change",
+                        e.proxy(function(t) {
+                                t.stopPropagation();
+                                var n = e(t.target).closest('li');
+                                var r = n.nextUntil("li.multiselect-group");
+                                var i = true;
+                                var s = r.find("input");
+                                s.each(function() {
+                                    i = i && e(this).prop("checked")
+                                });
+                                s.prop("checked", !i).trigger("change")
+                            },
+                            this));
+                e("li.multiselect-all",
+                        this
+                        .$ul)
+                    .css('background', '#f3f3f3')
+                    .css('border-bottom', '1px solid #eaeaea');
+                e("li.multiselect-group > a, li.multiselect-all > a > label.checkbox", this.$ul)
+                    .css('padding', '3px 20px 3px 35px');
+                e("li.multiselect-group > a > input", this.$ul).css('margin', '4px 0px 5px -20px');
+            }
+        };
+    }());
 
     $(function() {
         $("select[data-role=multiselect]").multiselect();
